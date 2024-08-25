195c038a66b04ceb24bb3a3ce941836ffb2c7e44
nader3254 <naderhany638@gmail.com>
2024-08-26 00:06:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar/component'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Navbar/>
+      
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/logo.jsx
@@ -0,0 +1,16 @@
+// import DarkLogo from './assets/logo_dark.png';
+// import LightLogo from './assets/logo_white.png';
+
+
+
+const Logo = () => {
+    return (
+      <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="/public/logo_dark.png" className="h-8" alt="Flowbite Logo" />
+        {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
+      </a>
+    );
+  };
+  
+  export default Logo;
+  
